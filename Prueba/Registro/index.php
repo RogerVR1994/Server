66c2160e75d4aaf535a1afd19dca88a4739e4140
Rogelio Ventura <rogeliovr94@gmail.com>
2017-04-17 18:55:59 -0500

    Quinta Prueba Registro Bayer

--- Prueba/Registro/index.php
+++ Prueba/Registro/index.php
@@ -24,7 +24,7 @@
 		  if($result!==FALSE){
 		     while($row = $result->fetch_assoc()) {
 		        printf("<tr><td> &nbsp;%s </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td></tr>", 
-		           $row["Fecha"], $row["Temperatura"]);
+		           $row["Fecha"], $row["Lote"]);
 		     }
 		     $link->close();
 		  }
